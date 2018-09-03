0e59685bdfd55118bd968547faa76c2bc10fcab6
Alexandru Branza <branza.alex@gmail.com>
2018-09-03 22:56:24 +0300

    Use Default Desktop File on Linux If Available

--- server/utils/register.js
+++ server/utils/register.js
@@ -16,25 +16,35 @@ var notify = () => {
 var register = {};
 
 register._writeDesktopFile = cb => {
-    var powderPath = process.execPath.substr(0,process.execPath.lastIndexOf("/")+1);
-    fs.writeFile(dataPath + '/powderweb.desktop', '[Desktop Entry]\n'+
-        'Version=1.0\n'+
-        'Name=PowderWeb\n'+
-        'Comment=Powder Web is a torrent streaming client with a Web UI\n'+
-        'Exec=' + process.execPath + ' %U\n'+
-        'Path=' + powderPath + '\n'+
-        'Icon=' + powderPath + 'icon.png\n'+
-        'Terminal=false\n'+
-        'Type=Application\n'+
-        'MimeType=application/x-bittorrent;x-scheme-handler/magnet;\n'+
-        '', cb);
+    const defaultLoc = '/usr/share/applications/PowderWeb.desktop'
+    fs.stat(defaultLoc, function(err, stat) {
+        if(err == null) {
+            cb(defaultLoc)
+        } else {
+            var powderPath = process.execPath.substr(0,process.execPath.lastIndexOf("/")+1);
+            fs.writeFile(dataPath + '/powderweb.desktop', '[Desktop Entry]\n'+
+                'Version=1.0\n'+
+                'Name=PowderWeb\n'+
+                'Comment=Powder Web is a torrent streaming client with a Web UI\n'+
+                'Exec=' + process.execPath + ' %U\n'+
+                'Path=' + powderPath + '\n'+
+                'Icon=' + powderPath + 'icon.png\n'+
+                'Terminal=false\n'+
+                'Type=Application\n'+
+                'MimeType=application/x-bittorrent;x-scheme-handler/magnet;x-scheme-handler/sop;x-scheme-handler/acestream;x-scheme-handler/http;x-scheme-handler/https;\n'+
+                '', (err) => {
+                    if (err) cb(null, err)
+                    else cb(dataPath)
+                })
+        }
+    })
 };
 
 register.torrent = () => {
     if (process.platform == 'linux') {
-        this._writeDesktopFile(err => {
+        this._writeDesktopFile((deskLoc, err) => {
             if (err) throw err;
-            var desktopFile = dataPath+'/powderweb.desktop';
+            var desktopFile = deskLoc;
             var tempMime = 'application/x-bittorrent';
             child.exec('gnome-terminal -x bash -c "echo \'Associating Files or URLs with Applications requires Admin Rights\'; echo; sudo echo; sudo echo \'Authentication Successful\'; sudo echo; sudo mv -f '+desktopFile+' /usr/share/applications; sudo xdg-mime default powderweb.desktop '+tempMime+'; sudo gvfs-mime --set '+tempMime+' powderweb.desktop; echo; echo \'Association Complete! Press any key to close ...\'; read" & disown');
         });
@@ -50,9 +60,9 @@ register.torrent = () => {
 
 register.link = (linkType) => {
     if (process.platform == 'linux') {
-        this._writeDesktopFile(err => {
+        this._writeDesktopFile((deskLoc, err) => {
             if (err) throw err;
-            var desktopFile = dataPath+'/powderweb.desktop';
+            var desktopFile = deskLoc;
             var tempMime = 'x-scheme-handler/' + linkType;
             child.exec('gnome-terminal -x bash -c "echo \'Associating Files or URLs with Applications requires Admin Rights\'; echo; sudo echo; sudo echo \'Authentication Successful\'; sudo echo; sudo mv -f '+desktopFile+' /usr/share/applications; sudo xdg-mime default powderweb.desktop '+tempMime+'; sudo gvfs-mime --set '+tempMime+' powderweb.desktop; echo; echo \'Association Complete! Press any key to close ...\'; read" & disown');
         });
