28c0b4a1e4263cb3a1f8b30e9f979e8d0dd35fed
Jaruba Firanelli <branza.alex@gmail.com>
2019-02-02 19:10:45 +0200

    Use HTTPS When Checking for Updates

--- server/utils/updater.js
+++ server/utils/updater.js
@@ -15,7 +15,7 @@ module.exports = {
 
             // announce update every 3 days on start-up
             if (updateCheck < Math.floor(Date.now() / 1000) - 259200) {
-                needle.get('http://powder.media/web-version', (err, res) => {
+                needle.get('https://powder.media/web-version', (err, res) => {
                     if (!err && res.body && res.body.length < 100 && res.body.includes && res.body.includes('|')) {
                         const vers = (Buffer.isBuffer(res.body) ? res.body.toString() : res.body).split('|')
                         if (vers[0] == version) {
