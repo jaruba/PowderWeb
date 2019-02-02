299821f32b9a4593f9f0f43bc5fac72326c9007e
Jaruba Firanelli <branza.alex@gmail.com>
2019-02-02 19:03:51 +0200

    Show Complete Version in Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer () {
   return (
     <Wrapper className="nodragwindow footerContent">
       <div><a className="donateButton" target="_blank" href="https://powder.media/donate">Donate</a></div>
-      <div>Powder Web v0.8</div>
+      <div>Powder Web v0.8.0</div>
     </Wrapper>
   )
 }
