7c20c22a4e0e30e318ed23c1ffa5860c724b8f61
Alexandru Branza <branza.alex@gmail.com>
2020-07-16 21:58:56 +0300

    Fix "Download Playlist" on Remote Devices

--- server/streams.js
+++ server/streams.js
@@ -85,6 +85,7 @@ const isRedirectToMagnet = (url, cb) => {
 }
 
 const basicTorrentData = (torrent, cb) => {
+    torrent = Buffer.isBuffer(torrent || '') ? torrent.toString() : torrent
     console.log('basic torrent data')
     console.log(torrent)
     if (!torrent || !torrent.startsWith) {
